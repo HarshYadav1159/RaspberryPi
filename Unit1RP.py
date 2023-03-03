9d704b84d5f26b6af9f598900ec8d3743ff65dca
HarshYadav1159 <92823990+HarshYadav1159@users.noreply.github.com>
2023-03-03 22:02:06 +0530

    Add files via upload

---
+++ Unit1RP.py
@@ -0,0 +1,10 @@
+# Quetion 1:
+list1=["M", 'na',"i", "Ke"]
+list2= ["y","me","s", "lly"]
+for i in range(len(list1)):
+    list1[i] = list1[i]+list2[i]
+print(list1)
+
+# Question 2:
+list1=[10,20,30,40]
+list2= [100, 200, 300, 400]
